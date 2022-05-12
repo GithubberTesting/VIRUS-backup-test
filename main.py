38f74b45ddef39a29ba3d06965fe61528c2ab9d6
GithubberTesting <105431157+GithubberTesting@users.noreply.github.com>
2022-05-12 20:24:40 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print("\033[31mWELCOME TO THE VIRUS")
+
+ekey='give_link'
+ekeyget=input("\033[37mSAY \033[34mgive_link\033[37m: ")
+if ekeyget==ekey:
+     print("\033[34mL1NK: \033[31mhttps://bit.ly/3ild93L")
+else:
+    print("INVALID KEY!")
+
+print("\033[37m[INFO]")
+
+ikey='see_info'
+ikeyget=input("TYPE:see_info: ")
+if ikeyget==ikey:
+     print("INFO:\033[32mPlease use this for educational purposes only.We will not be responsible for any kind of loss or damage you do to others. ")
+
+else:
+    print("INVALID COMMAND!")
