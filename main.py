2528cbf207910d527c97f17819dafb14f51b116d
GithubberTesting <105431157+GithubberTesting@users.noreply.github.com>
2022-05-12 21:01:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("\033[31mWELCOME TO THE VIRUS")
 
 ekey='give_link'
-ekeyget=input("\033[37mSAY \033[34mgive_link\033[37m: ")
+ekeyget=raw_input("\033[37mSAY \033[34mgive_link\033[37m: ")
 if ekeyget==ekey:
      print("\033[34mL1NK: \033[31mhttps://bit.ly/3ild93L")
 else:
@@ -10,7 +10,7 @@ else:
 print("\033[37m[INFO]")
 
 ikey='see_info'
-ikeyget=input("TYPE:see_info: ")
+ikeyget=raw_input("TYPE:see_info: ")
 if ikeyget==ikey:
      print("INFO:\033[32mPlease use this for educational purposes only.We will not be responsible for any kind of loss or damage you do to others. ")
 
